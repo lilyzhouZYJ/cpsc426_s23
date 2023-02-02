c6e73aab10def1abab8b3b10f9de82b53d635003
Lily Zhou <lzhouzyj@gmail.com>
2023-02-01 23:04:48 -0500

    A1 to A3 without discussion questions (02/01)

--- lab1/video_rec_service/server_lib/server_lib.go
+++ lab1/video_rec_service/server_lib/server_lib.go
@@ -6,6 +6,7 @@ import (
 	umc "cs426.yale.edu/lab1/user_service/mock_client"
 	pb "cs426.yale.edu/lab1/video_rec_service/proto"
 	vmc "cs426.yale.edu/lab1/video_service/mock_client"
+	upb "cs426.yale.edu/lab1/user_service/proto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -52,8 +53,61 @@ func (server *VideoRecServiceServer) GetTopVideos(
 	ctx context.Context,
 	req *pb.GetTopVideosRequest,
 ) (*pb.GetTopVideosResponse, error) {
-	return nil, status.Error(
-		codes.Unimplemented,
-		"VideoRecService: unimplemented!",
-	)
+
+	// Create gRPC channel
+	conn, err := grpc.Dial(server.options.UserServiceAddr, opts...)
+	if err != nil {
+		log.Fatalf("VideoRecService: fail to dial with error %v\n", err)
+		return nil, status.Errorf(
+			status.Code(err),
+			"VideoRecService: error %v\n",
+			err
+		)
+	}
+	defer conn.Close()
+
+	// Create client stub
+	client := pb.NewRouteGuideClient(conn)
+
+	// Create user client stub
+	userClient := upb.NewUserServiceClient(conn)
+
+	// Fetch user info on the original user
+	orig_user_id := req.GetUserId()
+	origUserResponse, err := userClient.GetUser(ctx, &upb.GetUserRequest{user_ids: orig_user_id})
+	if err != nil {
+		log.Fatalf("VideoRecService: fail to fetch user info on orig user with error %v", err)
+		return nil, status.Errorf(
+			status.Code(err),
+			"VideoRecService: error %v\n",
+			err
+		)
+	}
+	orig_user_infos []*UserInfo := origUserResponse.GetUsers()
+
+	// Fetch users that the orig user subscribes to
+	subscribed_to := make(uint64[], 0)
+	for _, orig_user_info := range orig_user_infos {
+		s := orig_user_info.GetSubscribedTo()
+		subscribed_to := append(subscribed_to, s)
+	}
+
+	// Find the liked videos of the subscribe users
+	likedVideoResponse, err := userClient.GetUser(ctx, &upb.GetUserRequest{user_ids: subscribed_to})
+	if err != nil {
+		log.Fatalf("VideoRecService: fail to fetch liked videos with error %v", err)
+		return nil, status.Errorf(
+			status.Code(err),
+			"VideoRecService: error %v\n",
+			err
+		)
+	}
+	subscribed_user_infos []*UserInfo := likedVideoResponse.GetUsers()
+	liked_videos := make(uint64[], 0)
+	for _, subscribed_user_info := range subscribed_user_infos {
+		vids := subscribed_user_info.GetLikedVideos()
+		liked_videos = append(liked_videos, vids)
+	}
+
+	// TODO: make sure there are no duplicates in liked_videos
 }
